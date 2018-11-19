3aef6c8126b10f2c49cdc6a49cc8be7a66438054
Malay Shah <malay95@users.noreply.github.com>
2018-11-18 18:59:41 -0700

    kai bu

--- models/Favorites.js
+++ models/Favorites.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const likesSchema = new Schema({
+const FavoritesSchema = new Schema({
     _userId: {type: Schema.Types.ObjectId, ref : 'users'},
     _noteId: {type: Schema.Types.ObjectId, ref : 'notes'},
     timeStamp: {type: Date, default: Date.now}
 
 });
 
-mongoose.model('likes', likesSchema);
+mongoose.model('favorites', FavoritesSchema);
