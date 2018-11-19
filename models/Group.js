3e273122092698e2ea3b61e5b3d5f047f5c4b0e7
arshah12 <arshah12@asu.edu>
2018-11-18 18:19:55 -0700

    note added

--- models/Group.js
+++ models/Group.js
@@ -2,7 +2,11 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const groupSchema = new Schema({
-    userId: [{
+    userId : [{
+        type : mongoose.Schema.Types.ObjectId,
+        ref : 'users'
+    }],
+    noteId : [{
         type : mongoose.Schema.Types.ObjectId,
         ref : 'users'
     }],
