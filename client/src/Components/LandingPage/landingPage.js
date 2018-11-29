e2cc9dfab1d60e9944076c2d022f2b5e6f70d366
dharmingd <dharmindholiya@gmail.com>
2018-11-29 00:47:08 -0700

    Implemented Landingpage

--- client/src/Components/LandingPage/landingPage.js
+++ client/src/Components/LandingPage/landingPage.js
@@ -1,30 +1,37 @@
 import React, { Component } from "react";
 import image from "../../study.png";
+import signin from '../../signIn.png';
+import {NavLink} from 'react-router-dom';
 import "./landingPage.css";
 
 class LandingPage extends Component {
   render() {
-    return (
-      <div class="page">
-        <div className="left">
-          <img src={image} className="img" />
-        </div>
-        <div className="right">
-          <h2> Welcome to Study Genie!</h2>
-          <h4>Genie that helps ease education</h4>
-          <div>
-            It unifies all your study materials together, making your
-            preparation better, study a lot better. Seamlessly integrate your
-            notes, cheatsheets, view, collaborate with your classmates. Find
-            everything in one place!
+      return (
+          <div className="page">
+              <div className="left">
+                  <img src={image} className="img"/>
+              </div>
+              <div className="right">
+                  <h2> Welcome to Study Genie!</h2>
+                  <h4>Genie that helps ease education</h4>
+                  <div>
+                      It unifies all your study materials together, making your preparation
+                      better, study a lot better. Seamlessly integrate your notes, cheatsheets,
+                      view, collaborate with your classmates. Find everything in one place!
+                  </div>
+                  <div>
+                      View your top recommendations, favourite the notes you find useful,
+                      find notes based on your useful topics!.
+                  </div>
+                  <br/><br/>
+                  <div>
+                      <a href="/auth/google"><img src={signin} className="signIn"/></a>
+                  </div>
+              </div>
+
           </div>
-          <div>
-            View your top recommendations, favourite the notes you find useful,
-            find notes based on your useful topics!.
-          </div>
-        </div>
-      </div>
-    );
+
+      );
   }
 }
 
